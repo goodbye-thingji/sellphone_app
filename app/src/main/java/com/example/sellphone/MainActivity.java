d81012b814ec685f19c3c89115b816f523c1e6ef
13circle <13circle97@gmail.com>
2021-07-20 20:33:58 +0900

    feature: Android Google Sign-In Authentication

--- app/src/main/java/com/example/sellphone/MainActivity.java
+++ app/src/main/java/com/example/sellphone/MainActivity.java
@@ -10,17 +10,31 @@ import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.GridView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.widget.Toolbar;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
 public class MainActivity<phone1> extends AppCompatActivity {
     private static final String TAG = "Main_Activity";
+    private static final int RC_SIGN_IN = 5252;
 
     private ImageView ivMenu;
     private DrawerLayout drawerLayout;
@@ -28,6 +42,9 @@ public class MainActivity<phone1> extends AppCompatActivity {
     private long lastTime;
     ImageView phone, phone2, phone3, phone4, phone5, phone6, phone7, phone8, phone9, phone10, phone11, phone12;
 
+    GoogleSignInOptions gso;
+    GoogleSignInClient mGoogleSignInClient;
+    GoogleSignInAccount account;
 
     @SuppressLint("WrongViewCast")
     @Override
@@ -72,6 +89,19 @@ public class MainActivity<phone1> extends AppCompatActivity {
                 drawerLayout.openDrawer(Gravity.LEFT);
             }
         });
+
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.server_client_id))
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        account = GoogleSignIn.getLastSignedInAccount(this);
+        if (account != null) {
+            Toast.makeText(getApplicationContext(), "반갑습니다, " + account.getDisplayName() + " 님", Toast.LENGTH_SHORT).show();
+        } else {
+            Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+            startActivityForResult(signInIntent, RC_SIGN_IN);
+        }
     }
     public void onClick(View view) {
         switch (view.getId()) {
@@ -125,6 +155,31 @@ public class MainActivity<phone1> extends AppCompatActivity {
                 startActivity(it12);
                 break;
 
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            account = completedTask.getResult(ApiException.class);
+            // TODO: Server-side Authentication of account.getIdToken()
+            Toast.makeText(getApplicationContext(), "환영합니다, " + account.getDisplayName() + " 님", Toast.LENGTH_SHORT).show();
+        } catch (ApiException e) {
+            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+            Toast.makeText(
+                    getApplicationContext(),
+                    "알 수 없는 이유로 로그인에 실패했습니다.\n오류 코드: " + e.getStatusCode(),
+                    Toast.LENGTH_SHORT
+            ).show();
         }
     }
 
